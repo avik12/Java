ac23a2074d81fad14c816fef9caacb8306fbe74e
avik.12 <avik.brahma@gmail.com>
2017-05-16 08:57:55 +0530

    first commit

---
+++ Switch.java
@@ -0,0 +1,20 @@
+
+public class Switch {
+
+	public static void main (String args[])
+	{
+		int x = 5;
+		switch (x) 
+		{
+		case 1:System.out.println("HOHO");
+		break;
+		case 2:System.out.println("HAHHA");
+		break;
+		case 3:System.out.println("HUHU");
+		break;
+		default:System.out.println("LALA");
+		break;
+		}
+		
+	}
+}
